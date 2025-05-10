3e817a3d480f5bf6eb40f91a246e93956d0d083b
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-05-10 11:04:13 +0700

    redo AuthContext logic
    - Validate token on mount using useEffect.
    - Decode user ID from JWT and store it in user state.
    - Expose user object and setUser in AuthContext.
    - Replace isAuthenticated boolean with derived value from user.

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,8 +1,16 @@
-import { ReactNode, createContext, useState, useContext } from "react";
+import React, {
+  ReactNode,
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+} from "react";
 import { jwtDecode } from "jwt-decode";
 
 type AuthContext = {
   isAuthenticated: boolean;
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
   signIn: (token: string) => void;
   signOut: () => void;
 };
@@ -11,40 +19,50 @@ type AuthProviderProps = {
   children: ReactNode;
 };
 
+type User = {
+  id: string | null;
+};
+
 type JwtPayload = {
   exp: number;
+  id: string;
 };
 
 const AuthContext = createContext<AuthContext | undefined>(undefined);
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const isTokenValid = () => {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
     const token = localStorage.getItem("token");
-    if (!token) return false;
+    if (!token) return;
     try {
       const decodedToken = jwtDecode<JwtPayload>(token);
-      return decodedToken.exp * 1000 > Date.now(); // Return true if token is not expired yet, false otherwise
+      const isTokenExpired = decodedToken.exp * 1000 > Date.now(); // Return true if token is not expired yet, false otherwise
+      if (isTokenExpired) {
+        localStorage.removeItem("token");
+      } else {
+        setUser({ id: decodedToken.id });
+      }
     } catch (error) {
       console.log(error);
-      return false;
     }
-  };
-
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    isTokenValid() // True if token exists and not expired, false otherwise (null)
-  );
+  }, []);
 
   const signIn = (token: string) => {
     localStorage.setItem("token", token);
-    setIsAuthenticated(true);
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    setUser({ id: decodedToken.id });
   };
 
   const signOut = () => {
     localStorage.removeItem("token");
-    setIsAuthenticated(false);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn, signOut }}>
+    <AuthContext.Provider
+      // !!user means: If user exists it will be true
+      value={{ isAuthenticated: !!user, user, setUser, signIn, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
