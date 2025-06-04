8bacc84785f6a8bff5e3653ee6071d5d7f6d70b7
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-06-04 21:48:03 +0700

    refactor post card component
    - Switch layout from flexbox to grid.
    - Add placeholder category link.
    - Use semantic elements (h2. header, section, footer).
    - Change "Uploaded on" to "Published on" and "Last updated on" to "Updated on".
    - Remove user avatar for now.
    - Remove redundant Tailwind classes.

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,47 +1,54 @@
 import type { PostCardProps } from "../types";
 import { Link } from "react-router";
-import { UserInitialIcon } from "./Icons";
 import { formatDate } from "../utils";
 
 const PostCard = ({ post }: PostCardProps) => {
   return (
-    <article className="flex flex-col gap-2 rounded-xl bg-gray-200">
-      <div className="flex flex-col">
-        <div className="flex flex-col gap-[6px] px-4 py-2">
+    <article className="grid grid-rows-[auto_1fr_auto] rounded-xl bg-gray-200">
+      <header className="flex flex-col gap-1 border-b border-b-gray-300 p-4">
+        <h2 title={post.title} className="line-clamp-3">
           <Link
             to={`/posts/${post.id}`}
-            className="self-start text-lg font-semibold transition-all duration-200 hover:text-blue-400"
+            className="text-md inline font-semibold transition-colors hover:text-blue-400"
           >
             {post.title}
           </Link>
+        </h2>
 
-          <div className="flex items-center gap-1">
-            <UserInitialIcon userName={post.author.name} size="xs" />
-            <Link
-              to={`/profile/${post.authorId}`}
-              className="text-sm text-black/60 transition-all duration-200 hover:text-blue-400"
-            >
-              {post.author.name}
-            </Link>
-          </div>
-
-          <p className="line-clamp-4 overflow-hidden text-sm leading-relaxed text-ellipsis">
-            {post.content}
-          </p>
-
+        <div className="flex items-center gap-1">
+          <span className="text-sm text-black/60">In</span>
           <Link
-            to={`/posts/${post.id}`}
-            className="self-end rounded-xl bg-blue-400 px-2 py-1 text-sm text-white transition-all duration-200 hover:bg-blue-400/80"
+            to=""
+            className="text-sm transition-colors duration-200 first-letter:capitalize hover:text-blue-400"
           >
-            Continue reading <span>&#10230;</span>
+            {post.category}
+          </Link>
+          <span className="text-sm text-black/60">by</span>
+          <Link
+            to={`/profile/${post.authorId}`}
+            className="transition-color text-sm duration-200 hover:text-blue-400"
+          >
+            {post.author.name}
           </Link>
         </div>
-      </div>
+      </header>
 
-      <div className="flex flex-col items-end px-4 py-2 text-sm text-black/60">
-        <p>Uploaded on {formatDate(post.createdAt)}</p>
-        <p>Last updated on {formatDate(post.updatedAt)}</p>
-      </div>
+      <section className="flex flex-col gap-2 p-4">
+        <p className="mb-auto line-clamp-4 text-sm leading-relaxed">
+          {post.content}
+        </p>
+        <Link
+          to={`/posts/${post.id}`}
+          className="transition-color self-end rounded-xl bg-blue-400 px-2 py-1 text-sm text-white duration-200 hover:bg-blue-400/80"
+        >
+          Continue reading <span>&#10230;</span>
+        </Link>
+      </section>
+
+      <footer className="flex flex-col items-end border-t border-t-gray-300 p-4 text-sm text-black/60">
+        <p>Published on {formatDate(post.createdAt)}</p>
+        <p>Updated on {formatDate(post.updatedAt)}</p>
+      </footer>
     </article>
   );
 };
