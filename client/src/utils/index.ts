9b204cfb31b6fccab9543b33d8c23d03d011f289
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-06-06 21:51:19 +0700

    add hour and minute to the date format

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,7 +1,12 @@
 export const formatDate = (date: string | number | Date) => {
-  return new Date(date).toLocaleString("en-GB", {
+  const formattedDate = new Date(date).toLocaleString("en-GB", {
     day: "numeric",
     month: "long",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
   });
+
+  return formattedDate.replace(" at ", ", ");
 };
