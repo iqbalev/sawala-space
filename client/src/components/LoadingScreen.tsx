dbfeb47bd3df8fcb8a9b10ff2937aa85c4b6abb1
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-06-08 22:15:50 +0700

    add spinner to loading screen component

--- client/src/components/LoadingScreen.tsx
+++ client/src/components/LoadingScreen.tsx
@@ -3,9 +3,10 @@ import type { LoadingScreenProps } from "../types";
 const LoadingScreen = ({ className }: LoadingScreenProps) => {
   return (
     <div
-      className={`flex min-h-[45dvh] items-center justify-center ${className}`}
+      className={`flex min-h-[45dvh] items-center justify-center gap-[1ch] ${className}`}
     >
-      <p>Loading...</p>
+      <div className="h-6 w-6 animate-spin rounded-full border-4 border-gray-300 border-t-blue-400"></div>
+      <span className="text-sm">Loading...</span>
     </div>
   );
 };
