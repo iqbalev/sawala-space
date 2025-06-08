e4d6538e630a544a6778ed0502269a258e599568
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-06-08 22:34:29 +0700

    add calculateReadTime, shorten month format

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,7 +1,7 @@
 export const formatDate = (date: string | number | Date) => {
   const formattedDate = new Date(date).toLocaleString("en-GB", {
     day: "numeric",
-    month: "long",
+    month: "short",
     year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
@@ -9,4 +9,21 @@ export const formatDate = (date: string | number | Date) => {
   });
 
   return formattedDate.replace(" at ", ", ");
+};
+
+export const calculateReadTime = (content: string) => {
+  const WORDS_PER_MINUTE = 250;
+  // These regex meant to remove all HTML tags and multi-empty spaces from being calculated
+  const normalizedContent = content
+    .replace(/<[^>]*>/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+  const wordCount = normalizedContent.split(" ").length;
+  const readTime = wordCount / WORDS_PER_MINUTE;
+
+  if (readTime < 0.5) {
+    return "Less than 1 min read";
+  } else {
+    return `${Math.ceil(readTime).toString()} min read`;
+  }
 };
